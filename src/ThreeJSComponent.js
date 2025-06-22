4493fcfd8ea8f0274a241c22763e65d5bd18b9da
devhnlee <benjamin.hn.lee@gmail.com>
2025-06-22 17:28:18 +0900

    Hadle highlight in AxonView

--- src/ThreeJSComponent.js
+++ src/ThreeJSComponent.js
@@ -9,7 +9,7 @@ export default function ThreeJSComponent() {
     const iframeRef = useRef(null)
     const cameraRef = useRef(null)
     const domain = "https://novel-head-392156.framer.app/"
-    const [iframeVisible, setIframeVisible] = useState(true);
+    const iframeVisible = useRef(true)
     const [hotspotVisible, setHotspotVisible] = useState(false);
     const [hotspots, setHotspots] = useState([]);
 
@@ -53,7 +53,7 @@ export default function ThreeJSComponent() {
     };
 
     const setAxonView = () => {
-        setIframeVisible(false)
+        iframeVisible.current = false
         setHotspotVisible(true)
         moveCamera(new THREE.Vector3(3.16, 3.25, 3.2), new THREE.Vector3(0, 0, 0))
     }
@@ -177,7 +177,7 @@ export default function ThreeJSComponent() {
                 mouse.x = (event.clientX / window.innerWidth) * 2 - 1
                 mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
 
-                if (iframeVisible) return
+                if (iframeVisible.current) return
                 raycaster.setFromCamera(mouse, camera)
                 const intersects = raycaster.intersectObjects(models)
 
@@ -206,56 +206,56 @@ export default function ThreeJSComponent() {
             }
             window.addEventListener("mousemove", onMouseMove)
 
-            const onMouseClick = (event) => {
-                raycaster.setFromCamera(mouse, camera)
-                const intersects = raycaster.intersectObjects(models)
+            // const onMouseClick = (event) => {
+            //     raycaster.setFromCamera(mouse, camera)
+            //     const intersects = raycaster.intersectObjects(models)
 
-                if (intersects.length > 0 && !iframeVisible) {
-                    const redirectPath =
-                        intersects[0].object.userData.redirectPath
-                    if (selectedObject === intersects[0].object) {
-                        window.location.href = domain + redirectPath
-                        return
-                    }
-                    let container2 = document.createElement("iframe")
-                    if (redirectPath !== "" && !selectedObject) {
-                        if (!iframeRef.current) {
-                            iframeRef.current = document.createElement("div")
-                            iframeRef.current.appendChild(container2)
-                            container2.style.width = "100%"
-                            container2.style.height = "100%"
-                            iframeRef.current.setAttribute(
-                                "id",
-                                "subpage_iframe"
-                            )
-                            iframeRef.current.style.position = "fixed"
-                            iframeRef.current.style.border = "1px solid #ccc"
-                            iframeRef.current.style.boxShadow =
-                                "0 4px 8px rgba(0, 0, 0, 0.1)"
-                            iframeRef.current.style.zIndex = "1000"
-                            document.body.appendChild(iframeRef.current)
-                        }
-                        container2.src = domain + redirectPath
-                        iframeRef.current.style.display = "block"
-                        setIframeVisible(true)
-                        const tempObject = intersects[0].object
-                        selectedObject = tempObject
+            //     if (intersects.length > 0 && !iframeVisible) {
+            //         const redirectPath =
+            //             intersects[0].object.userData.redirectPath
+            //         if (selectedObject === intersects[0].object) {
+            //             window.location.href = domain + redirectPath
+            //             return
+            //         }
+            //         let container2 = document.createElement("iframe")
+            //         if (redirectPath !== "" && !selectedObject) {
+            //             if (!iframeRef.current) {
+            //                 iframeRef.current = document.createElement("div")
+            //                 iframeRef.current.appendChild(container2)
+            //                 container2.style.width = "100%"
+            //                 container2.style.height = "100%"
+            //                 iframeRef.current.setAttribute(
+            //                     "id",
+            //                     "subpage_iframe"
+            //                 )
+            //                 iframeRef.current.style.position = "fixed"
+            //                 iframeRef.current.style.border = "1px solid #ccc"
+            //                 iframeRef.current.style.boxShadow =
+            //                     "0 4px 8px rgba(0, 0, 0, 0.1)"
+            //                 iframeRef.current.style.zIndex = "1000"
+            //                 document.body.appendChild(iframeRef.current)
+            //             }
+            //             container2.src = domain + redirectPath
+            //             iframeRef.current.style.display = "block"
+            //             setIframeVisible(true)
+            //             const tempObject = intersects[0].object
+            //             selectedObject = tempObject
 
-                        updateIframeStyle()
-                    } else if (redirectPath === "" && iframeRef.current) {
-                        iframeRef.current.style.display = "none"
-                        setIframeVisible(false)
-                        selectedObject = null
-                    }
-                } else {
-                    if (iframeRef.current) {
-                        iframeRef.current.style.display = "none"
-                    }
-                    selectedObject = null
-                    setIframeVisible(true)
-                }
-            }
-            window.addEventListener("click", onMouseClick)
+            //             updateIframeStyle()
+            //         } else if (redirectPath === "" && iframeRef.current) {
+            //             iframeRef.current.style.display = "none"
+            //             setIframeVisible(false)
+            //             selectedObject = null
+            //         }
+            //     } else {
+            //         if (iframeRef.current) {
+            //             iframeRef.current.style.display = "none"
+            //         }
+            //         selectedObject = null
+            //         setIframeVisible(true)
+            //     }
+            // }
+            // window.addEventListener("click", onMouseClick)
 
             const onMouseDoubleClick = (event) => {
                 raycaster.setFromCamera(mouse, camera)
@@ -306,7 +306,7 @@ export default function ThreeJSComponent() {
             return () => {
                 window.removeEventListener("resize", onWindowResize)
                 window.removeEventListener("mousemove", onMouseMove)
-                window.removeEventListener("click", onMouseClick)
+                // window.removeEventListener("click", onMouseClick)
                 window.removeEventListener("dblclick", onMouseDoubleClick)
                 window.removeEventListener("keydown", onKeyDown)
                 if (iframeRef.current) {
@@ -318,7 +318,7 @@ export default function ThreeJSComponent() {
         }
 
         init()
-    }, [])
+    }, [iframeVisible])
 
     return (
     <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
